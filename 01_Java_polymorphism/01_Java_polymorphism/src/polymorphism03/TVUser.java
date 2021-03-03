4f1967b6f0a66f4e957a45158ca999a4e481027b
devnewbiee <custom94@naver.com>
2021-03-03 10:28:53 +0900

    git setting

--- 01_Java_polymorphism/01_Java_polymorphism/src/polymorphism03/TVUser.java
+++ 01_Java_polymorphism/01_Java_polymorphism/src/polymorphism03/TVUser.java
@@ -7,11 +7,14 @@ public class TVUser {
 		
 		//삼성TV 사용
 		//TV tv = (TV)factory.getBean("samsung");
-		TV tv = (TV)factory.getBean(args[0]);
-		tv.powerOn();
-		tv.volumnUp();
-		tv.volumnDown();
-		tv.powerOff();
+		
+		for (int i = 0; i < args.length; i++) {
+			TV tv = (TV)factory.getBean(args[i]);
+			tv.powerOn();
+			tv.volumnUp();
+			tv.volumnDown();
+			tv.powerOff();
+		}
 
 	}
 
